dc779dab2847c8546c14b7849be5b712be1b2c3e
LucaSpennato <luca.spennato@gmail.com>
2022-09-09 16:01:49 +0200

    migration success

---
+++ database/migrations/2022_09_09_132936_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('agency');
+            $table->string('departure_station');
+            $table->string('arrival_station');
+            $table->date('departure_time');
+            $table->date('arrival_time');
+            $table->string('train_code');
+            $table->smallInteger('number_of_carriages');
+            $table->boolean('is_on_time')->default(true);
+            $table->boolean('is_cancelled')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
