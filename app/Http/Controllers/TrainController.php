02fb50a1d028143a640ebf68d6a1c41913dcb12c
LucaSpennato <luca.spennato@gmail.com>
2022-09-09 17:44:51 +0200

    print on page

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Train;
 use Illuminate\Http\Request;
 
 class TrainController extends Controller
 {
-    //
+    public function index(){
+        $trains = Train::all();
+        return view('home', compact('trains'));
+    }
 }
