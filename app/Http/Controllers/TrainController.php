5c11ecd7f2f9cc4a615820ca27b0e39496c35d71
LucaSpennato <luca.spennato@gmail.com>
2022-09-09 18:02:42 +0200

    print trains departing today

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class TrainController extends Controller
 {
     public function index(){
-        $trains = Train::all();
+        $trains = Train::where('departure_time', 'like', '2022-09-09%')->get();
         return view('home', compact('trains'));
     }
 }
